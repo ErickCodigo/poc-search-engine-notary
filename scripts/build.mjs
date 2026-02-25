e4642c485c6bdefa2cf9e5f84f7f6fba894b3889
Erick Hilario <erprogramador@gmail.com>
2026-02-25 00:50:54 -0500

    chore: Generar un html en español y documentar proceso de convertir mdx a html

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -10,15 +10,16 @@ const mdxContent = await readFile('src/index.mdx', 'utf-8')
 
 const now = new Date();
 const datePart = now.toLocaleDateString('en-GB');
-const timePart = now.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+const timePart = now.toLocaleTimeString(['es-PE'], {hour: '2-digit', minute: '2-digit'});
 
 const formattedDateTime = `${datePart} ${timePart}`;
 
+// Unified trabaja con Abstract Syntax Tree (AST)
 const file = await unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeDocument, {title: `Actualizaste el ${formattedDateTime}`})
-    .use(rehypeStringify)
+    .use(remarkParse) // Convierte a Markdown Abstract Syntax Tree (mAST)
+    .use(remarkRehype) // Convierte a HTML Abstract Syntax Tree (hAST)
+    .use(rehypeDocument, {language: 'es', title: `Actualizaste el ${formattedDateTime}`}) // Agrega head y body tags.
+    .use(rehypeStringify) // Convierte de AST a HTML.
     .process(mdxContent)
 
 await mkdir('docs', {recursive: true})
